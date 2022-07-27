b8cc7b5efed9dcd9420877131e920023e9600f93
saradevera <saradevv@gmail.com>
2022-07-27 09:56:39 +0200

    27_07

---
+++ cosas_api.py
@@ -0,0 +1,11 @@
+# IDEALISTA 🏠
+Ideal_apikey = 'con9ip2i1k9yep3n0njrlrlqu3zas4on'
+Ideal_secret = 'kv1iBZYQaI4z'
+
+
+# TWITTER 🐦
+Tw_apikey = '5fRorITZDDO8OEbSqkzgxooqK'
+Tw_apikeysecret = 'D8TFMHKP9D1nH7mnjfiVo1ieVItMaHIRNICuSzugqwVPf5HTIO'
+Tw_bearer_token = 'AAAAAAAAAAAAAAAAAAAAANpJfAEAAAAAljldtEwBryEPnL1s7xeSBNtGfOg%3DAlMelAoVLSGe9WkHMvkJTOmilaiEX34gPBZRmMyqPrfZARWppX'
+Tw_access_token = '302625608-DbjchNiKnl8N0XunSjfSrrly5GeSb1gLFuzDSAbf'
+Tw_access_token_secret = '8PpywZwae4rExi2ok3qVZgfkLuLwdjMKVygB6rPQdB4Xq'
